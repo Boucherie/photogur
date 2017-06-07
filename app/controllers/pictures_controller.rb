2eacc801acaadcd1bfc70ffbb69cd6ebf5f9f331
Emily Boucher <emi.boucher@gmail.com>
2017-06-07 17:13:58 -0400

    added new and create methods to controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,6 +12,17 @@ class PicturesController < ApplicationController
   end
 
   def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      #if the picture is saved generate a get request the '/pictures' (the index)
+      redirect_to "/pictures"
+    else
+      #otherwise render new.html.erb
+      render :new
+    end
   end
 
+  def picture_params
+    { title: params[:picture][:title], artist: params[:picture][:artist], url: params[:picture][:url] }
+  end
 end
