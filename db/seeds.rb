d7d5cf5dd3257ea6938fd0b96af7240dac5d0bea
Emily Boucher <emi.boucher@gmail.com>
2017-06-07 16:14:14 -0400

    added dynamic routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 Picture.create(
   title:  'The old church on the coast of White Sea',
   artist: 'Sergey Ershov',
-  url:    'http://bitmakerlabs.s3amazonaws.com/photogur/house.jpg'
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg'
 )
 
 Picture.create(
